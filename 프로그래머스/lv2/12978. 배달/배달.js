5685d644257cdc20dfb91922e51c003a8928d2d4
haengsong <97587274+haengsong@users.noreply.github.com>
2023-02-17 13:58:58 +0900

    [level 2] Title: 배달, Time: 1.14 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,36 +1,8 @@
 function solution(N, road, K) {
-    let answer = 0;
-    
-    const R = {};
-    for (let i=1; i<N+1; i++) {
-        R[i] = []
-    }
-    for ([a,b,c] of road) {
-        R[a].push([c,b])
-        R[b].push([c,a])
-    }
-    const Q = [[0,1]];
-    const ARR = new Array(N+1).fill(500001*N);
-    ARR[1] = 0;
-    
-    while (Q.length > 0) {
-        Q.sort();
-        [cnt,now] = Q.shift();
-        
-        for ([C,next] of R[now]){
-            if (ARR[next] > cnt + C) {
-                ARR[next] = cnt + C
-                Q.push([cnt+C,next])
-            }
-        }
-    }
-    
-    for (let i=1; i<N+1; i++) {
-        if (ARR[i] <= K) {
-            answer += 1
-        }
-    }
-    
+    var answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
 
     return answer;
 }
